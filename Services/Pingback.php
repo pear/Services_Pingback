c10090ce1de6af04a3d6b60a82d7b840f7506864
Firman Wandayandi <firman@php.net>
2007-08-03 11:14:03 +0000

    Updates license header and ready for next bugfix release 0.2.2
    git-svn-id: http://svn.php.net/repository/pear/packages/Services_Pingback/trunk@240670 c90b9560-bf6c-de11-be94-00142212c4b1

--- Services/Pingback.php
+++ Services/Pingback.php
@@ -12,45 +12,23 @@
  * PHP versions 4 and 5
  *
  * LICENSE:
- *
- * BSD License
- *
- * Copyright (c) 2005-2006 Firman Wandayandi <firman@php.net>
+ * Copyright (c) 2005-2007 Firman Wandayandi <firman@php.net>
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above
- *    copyright notice, this list of conditions and the following
- *    disclaimer in the documentation and/or other materials provided
- *    with the distribution.
- * 3. Neither the name of Firman Wandayandi nor the names of
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * This source file is subject to the BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.opensource.org/licenses/bsd-license.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to pear-dev@lists.php.net so we can send you a copy immediately.
  *
  * @category Web Services
  * @package Services_Pingback
  * @author Firman Wandayandi <firman@php.net>
- * @copyright Copyright (c) 2005-2006 Firman Wandayandi <firman@php.net>
+ * @copyright Copyright (c) 2005-2007 Firman Wandayandi <firman@php.net>
  * @license http://www.opensource.org/licenses/bsd-license.php
- *          BSD License
+ *          BSD license
  * @version CVS: $Id$
  */
 
@@ -154,9 +132,9 @@ $GLOBALS['_Services_Pingback_server'] = null;
  * @category Web Services
  * @package Services_Pingback
  * @author Firman Wandayandi <firman@php.net>
- * @copyright Copyright (c) 2005-2006 Firman Wandayandi <firman@php.net>
+ * @copyright Copyright (c) 2005-2007 Firman Wandayandi <firman@php.net>
  * @license http://www.opensource.org/licenses/bsd-license.php
- *          BSD License
+ *          BSD license
  * @version Release: @package_version@
  * @todo Enabled access denied and upstream error response.
  * @todo Extract more pretty context of source and flexible direction.
